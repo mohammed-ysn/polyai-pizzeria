dc9db5148f369862a62472989169b4222c906404
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-01-04 18:27:24 +0000

    Improve efficiency by visiting each block at most once per pizzeria

--- pizzeria_solution.py
+++ pizzeria_solution.py
@@ -1,10 +1,10 @@
-import itertools
 import numpy as np
 
 
 def main(debug=False):
     if debug:
         import sys
+
         sys.stdin = open("input.txt", "r")
 
     n, m = (int(x) for x in input().split())
@@ -14,34 +14,23 @@ def main(debug=False):
     city = np.zeros(shape=(n, n), dtype=int)
 
     for pizzeria in range(m):
-        x, y, r = (int(x) for x in input().split())
-        assert 1 <= x <= n
-        assert 1 <= y <= n
+        x_centre, y_centre, r = (int(x) for x in input().split())
+        assert 1 <= x_centre <= n
+        assert 1 <= y_centre <= n
         assert 1 <= r <= 5000
 
         # convert 1-based indexing to 0-based
-        x -= 1
-        y -= 1
+        x_centre -= 1
+        y_centre -= 1
 
-        pizzeria_reach = np.zeros(shape=(n, n), dtype=int)
-
-        for y_offset in range(r + 1):
-            for x_offset in range(r + 1):
-                if x_offset + y_offset > r:
-                    # exceeded max dist
+        # iterate through the square of length 2r + 1
+        # centred at the pizzeria
+        for y in range(max(y_centre - r, 0), min(y_centre + r + 1, n)):
+            for x in range(max(x_centre - r, 0), min(x_centre + r + 1, n)):
+                if abs(x - x_centre) + abs(y - y_centre) > r:
                     continue
 
-                # generate all block coordinates with the current x and y
-                # offsets in positive and negative directions
-                reachable_blocks = itertools.product(
-                    [y + y_offset, y - y_offset], [x + x_offset, x - x_offset]
-                )
-
-                for block_y, block_x in reachable_blocks:
-                    if 0 <= block_x < n and 0 <= block_y < n:
-                        pizzeria_reach[block_y][block_x] = 1
-
-        city += pizzeria_reach
+                city[y][x] += 1
 
     print(np.max(city))
 
